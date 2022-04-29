bcc226b40ec08125a7bcd8448f7e53ca928837d1
AliFarajzade <alifarajzade1@yahoo.com>
2022-04-29 07:11:49 -0400

    Add NProgress to _app.ts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
-import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast'
+import { RecoilRoot } from 'recoil'
 import theme from '../chakra/theme'
 import Layout from '../components/layout/layout.component'
-import { RecoilRoot } from 'recoil'
-import { Toaster } from 'react-hot-toast'
+import '../style.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const handleRouteStart = () => NProgress.start()
+        const handleRouteDone = () => NProgress.done()
+
+        Router.events.on('routeChangeStart', handleRouteStart)
+        Router.events.on('routeChangeComplete', handleRouteDone)
+        Router.events.on('routeChangeError', handleRouteDone)
+
+        return () => {
+            Router.events.off('routeChangeStart', handleRouteStart)
+            Router.events.off('routeChangeComplete', handleRouteDone)
+            Router.events.off('routeChangeError', handleRouteDone)
+        }
+    }, [])
+
     return (
         <RecoilRoot>
             <ChakraProvider theme={theme}>
