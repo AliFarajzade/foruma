317ca0e11012582e09a689b87e2d2f6c4960a404
AliFarajzade <alifarajzade1@yahoo.com>
2022-04-29 04:37:27 -0400

    Add 800 font weight.

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -2,6 +2,7 @@ import { extendTheme } from '@chakra-ui/react'
 import '@fontsource/open-sans/300.css'
 import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/700.css'
+import '@fontsource/open-sans/800.css'
 import Button from './button'
 
 const theme = extendTheme({
