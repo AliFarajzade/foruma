c1232a63ebe321eae43edd15a16e202213eddc20
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-14 09:39:48 -0400

    Add text inputs component to add post form.

--- src/components/post/new-post-form.component.tsx
+++ src/components/post/new-post-form.component.tsx
@@ -5,6 +5,7 @@ import { BsMic } from 'react-icons/bs'
 import { IoDocumentText, IoImagesOutline } from 'react-icons/io5'
 import { v4 as uuidv4 } from 'uuid'
 import TabItem from './tab-item.component'
+import TextInputs from './text-inputs.component'
 
 const formTabs = [
     {
@@ -27,7 +28,27 @@ const formTabs = [
 
 const NewPostForm: React.FC = () => {
     const [selectedTab, setSelectedTab] = useState<string>(formTabs[0].title)
+    const [formData, setFormData] = useState<{
+        title: string
+        description: string
+    }>({ title: '', description: '' })
+
     const handleChangeTab = (tabTitle: string) => setSelectedTab(tabTitle)
+
+    const handleFormChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) =>
+        setFormData(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+        }))
+
+    const handleSelectMedia = () => {}
+
+    const handleSubmitPost = async () => {}
+
+    console.log(formData)
+
     return (
         <Flex direction="column" bg="white" borderRadius={4} mt={2}>
             <Flex width="100%">
@@ -39,6 +60,15 @@ const NewPostForm: React.FC = () => {
                         handleChangeTab={handleChangeTab}
                     />
                 ))}
+            </Flex>
+            <Flex padding="20px" paddingBottom="10px">
+                {selectedTab === 'Post' && (
+                    <TextInputs
+                        formData={formData}
+                        handleFormChange={handleFormChange}
+                        handleSubmitPost={handleSubmitPost}
+                    />
+                )}
             </Flex>
         </Flex>
     )
