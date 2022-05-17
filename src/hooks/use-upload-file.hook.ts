bdb90d3d3510b8f955477827ffeb78f5c72bd530
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-17 09:24:22 -0400

    Create a custom hook for uploading a file into firebase storage.

---
+++ src/hooks/use-upload-file.hook.ts
@@ -0,0 +1,49 @@
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { useState } from 'react'
+import { storage } from '../firebase/config.firebase'
+
+const useUploadFile = (): [
+    { progress: number | null; isUploading: boolean },
+    (media: File, refString: string) => Promise<unknown>
+] => {
+    const [progress, setProgress] = useState<number | null>(null)
+    const [isUploading, setIsUploading] = useState<boolean>(false)
+
+    const uploadFile = async (media: File, refString: string) =>
+        new Promise((res, rej) => {
+            setIsUploading(true)
+            const storageRef = ref(storage, refString)
+
+            const uploadTask = uploadBytesResumable(storageRef, media)
+
+            uploadTask.on(
+                'state_changed',
+                snapshot => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    console.log('Upload is ' + progress + '% done')
+                    setProgress(progress)
+                },
+                storageError => {
+                    if (storageError) {
+                        setIsUploading(false)
+                        rej(storageError)
+                    }
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                        downloadURL => {
+                            console.log('File available at', downloadURL)
+                            setIsUploading(false)
+                            setProgress(null)
+                            res(downloadURL)
+                        }
+                    )
+                }
+            )
+        })
+
+    return [{ progress, isUploading }, uploadFile]
+}
+
+export default useUploadFile
