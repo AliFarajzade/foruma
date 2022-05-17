c8bad61458c5a2ef334cf55c8719fc3094b5b1dc
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-17 13:01:56 -0400

    Add use posts custom hook and post item component.

--- src/components/post/posts.component.tsx
+++ src/components/post/posts.component.tsx
@@ -1,3 +1,4 @@
+import { Stack } from '@chakra-ui/react'
 import {
     collection,
     getDocs,
@@ -8,11 +9,19 @@ import {
 } from 'firebase/firestore'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { firestore } from '../../firebase/config.firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { v4 as uuid } from 'uuid'
+import { auth, firestore } from '../../firebase/config.firebase'
+import usePosts from '../../hooks/use-posts.hook'
 import { TPost } from '../../types/post.types'
+import PostItem from './post-item.component'
 
 const Posts: React.FC = () => {
+    const [user] = useAuthState(auth)
+
     const { query: routeQuery } = useRouter()
+
+    const { postsState, setPostsState } = usePosts()
 
     const getPosts = async () => {
         const collectionRef = collection(firestore, 'posts')
@@ -30,6 +39,10 @@ const Posts: React.FC = () => {
                 ID: postObj.id,
                 ...(postObj.data() as Omit<TPost, 'ID'>),
             }))
+            setPostsState(prevState => ({
+                ...prevState,
+                posts,
+            }))
         } catch (error) {
             console.log(error)
         }
@@ -39,7 +52,18 @@ const Posts: React.FC = () => {
         getPosts()
     }, [])
 
-    return <h1>Posts</h1>
+    return (
+        <Stack spacing={2}>
+            {postsState.posts.map(postObj => (
+                <PostItem
+                    key={uuid()}
+                    post={postObj}
+                    isUserTheCreator={user?.uid === postObj.creatorID}
+                    userVoteValue={1}
+                />
+            ))}
+        </Stack>
+    )
 }
 
 export default Posts
