d5f2e0ff2e9b8fcec63a801577d691a64f2e851d
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-17 09:23:40 -0400

    Create type file for Post and add its type.

---
+++ src/types/post.types.ts
@@ -0,0 +1,16 @@
+import type { FieldValue, Timestamp } from 'firebase/firestore'
+
+export type TPost = {
+    ID: string
+    communityID: string
+    creatorID: string
+    creatorDisplayName: string
+    title: string
+    description: string
+    numberOfComments: number
+    voteStatus: number
+    mediaURL?: string
+    mediaType?: 'image' | 'video'
+    communityImageURL?: string
+    createdAt: Timestamp | FieldValue
+}
