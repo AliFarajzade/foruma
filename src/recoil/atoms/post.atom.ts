baef505cb2313f3ff7cd0f3edbef06053164d8a5
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-25 09:32:19 -0400

    Add new field for postsVote.

--- src/recoil/atoms/post.atom.ts
+++ src/recoil/atoms/post.atom.ts
@@ -1,15 +1,16 @@
 import { atom } from 'recoil'
-import { TPost } from '../../types/post.types'
+import { IPostVote, TPost } from '../../types/post.types'
 
 interface IPostState {
     selectedPost: null | TPost
     posts: TPost[]
-    // postVotes
+    postsVote: IPostVote[]
 }
 
 const INITIAL_STATE: IPostState = {
     selectedPost: null,
     posts: [],
+    postsVote: [],
 }
 
 const postsStateAtom = atom<IPostState>({
