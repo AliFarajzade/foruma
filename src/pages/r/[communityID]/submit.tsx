f7f3f5f0b2c52c0996640b2e77b583a601b1cb6f
Ali <alifarajzade1@yahoo.com>
2022-06-10 10:19:13 +0430

    Add about community component with loading skeleton.

--- src/pages/r/[communityID]/submit.tsx
+++ src/pages/r/[communityID]/submit.tsx
@@ -1,9 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import { NextPage } from 'next'
+import AboutCommunity from '../../../components/community/about-community.component'
 import CommunityPageLayout from '../../../components/layout/community-layout.component'
 import NewPostForm from '../../../components/post/new-post-form.component'
+import PostSkeleton from '../../../components/post/post-skeleton.component'
+import useCommunityData from '../../../hooks/use-community-data.hook'
 
 const SubmitPage: NextPage = () => {
+    const { communityState } = useCommunityData()
     return (
         <CommunityPageLayout>
             <>
@@ -12,7 +16,16 @@ const SubmitPage: NextPage = () => {
                 </Box>
                 <NewPostForm />
             </>
-            <>{/* About */}</>
+
+            <>
+                {communityState.currentCommunity ? (
+                    <AboutCommunity
+                        currentCommunity={communityState.currentCommunity}
+                    />
+                ) : (
+                    <PostSkeleton />
+                )}
+            </>
         </CommunityPageLayout>
     )
 }
