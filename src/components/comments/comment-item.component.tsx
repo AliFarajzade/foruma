da67442f301c22cf67903c087e73de54677f11f4
Ali <alifarajzade1@yahoo.com>
2022-06-11 09:22:25 +0430

    Create comment item component.

---
+++ src/components/comments/comment-item.component.tsx
@@ -0,0 +1,72 @@
+import { Box, Flex, Icon, Spinner, Stack, Text } from '@chakra-ui/react'
+import moment from 'moment'
+import { FaReddit } from 'react-icons/fa'
+import {
+    IoArrowDownCircleOutline,
+    IoArrowUpCircleOutline,
+} from 'react-icons/io5'
+import { TComment } from '../../types/comment.types'
+
+interface IProps {
+    comment: TComment
+    handleDeleteComment: (commentID: string) => void
+    loadingDelete: boolean
+    userID: string | undefined
+}
+
+const CommentItem: React.FC<IProps> = ({
+    comment,
+    userID,
+    handleDeleteComment,
+    loadingDelete,
+}) => {
+    console.log(comment)
+    return (
+        <Flex>
+            <Box mr={2}>
+                <Icon as={FaReddit} fontSize={35} color="gray.300" />
+            </Box>
+            <Stack spacing={1}>
+                <Stack direction="row" align="center" fontSize="pt">
+                    <Text fontSize="12pt" fontWeight={700}>
+                        {comment.creatorDisplayName}
+                    </Text>
+                    <Text fontSize="10pt" color="gray.600">
+                        {moment(new Date(comment.createdAt.seconds)).fromNow()}
+                    </Text>
+                </Stack>
+                <Text fontSize="11pt">{comment.comment}</Text>
+                <Stack
+                    pt={2}
+                    direction="row"
+                    align="center"
+                    cursor="pointer"
+                    color="gray.500"
+                >
+                    <Icon fontSize={22} as={IoArrowUpCircleOutline} />
+                    <Icon fontSize={22} as={IoArrowDownCircleOutline} />
+                    {userID === comment.creatorID && (
+                        <>
+                            <Text
+                                fontSize="10pt"
+                                _hover={{ color: 'blue.500' }}
+                            >
+                                Edit
+                            </Text>
+                            <Text
+                                fontSize="10pt"
+                                _hover={{ color: 'blue.500' }}
+                                onClick={() => handleDeleteComment(comment.ID)}
+                            >
+                                Delete
+                            </Text>
+                            {loadingDelete && <Spinner />}
+                        </>
+                    )}
+                </Stack>
+            </Stack>
+        </Flex>
+    )
+}
+
+export default CommentItem
