e3e65c453e4ae4715ed2d39c86cf0932ed604a6e
Ali <alifarajzade1@yahoo.com>
2022-06-11 20:41:11 +0430

    Update component to show community name and picture.

--- src/components/navbar/directory.component.tsx
+++ src/components/navbar/directory.component.tsx
@@ -1,5 +1,14 @@
 import { ChevronDownIcon } from '@chakra-ui/icons'
-import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react'
+import {
+    Flex,
+    Icon,
+    Img,
+    Menu,
+    MenuButton,
+    MenuList,
+    Text,
+} from '@chakra-ui/react'
+import { FaReddit } from 'react-icons/fa'
 import { TiHome } from 'react-icons/ti'
 import useDirectory from '../../hooks/use-directory.hook'
 import Communities from './communities.component'
@@ -22,14 +31,31 @@ const Directory: React.FC = () => {
             >
                 <Flex align="center" justify="space-between">
                     <Flex align="center">
-                        <Icon
-                            fontSize={24}
-                            mr={{ base: 1, md: 2 }}
-                            as={TiHome}
-                        />
+                        {directoryState.selectedMenuItem.imageURL ? (
+                            <Img
+                                src={directoryState.selectedMenuItem.imageURL}
+                                alt={directoryState.selectedMenuItem.name}
+                                boxSize={30}
+                                borderRadius="full"
+                                mr={{ base: 1, md: 2 }}
+                            />
+                        ) : directoryState.selectedMenuItem.name === 'Home' ? (
+                            <Icon
+                                fontSize={24}
+                                mr={{ base: 1, md: 2 }}
+                                as={TiHome}
+                            />
+                        ) : (
+                            <Icon
+                                fontSize={30}
+                                mr={{ base: 1, md: 2 }}
+                                color="blue.500"
+                                as={FaReddit}
+                            />
+                        )}
                         <Flex display={{ base: 'none', lg: 'flex' }}>
                             <Text fontWeight="600" mr="1">
-                                Home
+                                {directoryState.selectedMenuItem.name}
                             </Text>
                         </Flex>
                     </Flex>
