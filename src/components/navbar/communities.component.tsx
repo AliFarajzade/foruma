41dd5b78e11c10f42b3612c428e5e900edc30d04
Ali <alifarajzade1@yahoo.com>
2022-06-11 15:19:56 +0430

    Update communities drop-down to show joined communities.

--- src/components/navbar/communities.component.tsx
+++ src/components/navbar/communities.component.tsx
@@ -1,24 +1,90 @@
-import { Flex, Icon, MenuItem } from '@chakra-ui/react'
+import { Box, Flex, Icon, MenuItem, Skeleton, Text } from '@chakra-ui/react'
 import { useState } from 'react'
+import { FaReddit } from 'react-icons/fa'
 import { GrAdd } from 'react-icons/gr'
+import useCommunityData from '../../hooks/use-community-data.hook'
 import CreateCommunityModal from '../modal/commiunity/create-community-modal.component'
+import CommunityMenuItem from './community-menu-item.component'
 
 const Communities: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const {
+        isLoading,
+        communityState: { mySnippets },
+    } = useCommunityData()
     return (
         <>
             <CreateCommunityModal isOpen={isOpen} setIsOpen={setIsOpen} />
-            <MenuItem
-                width="100%"
-                fontSize="11pt"
-                _hover={{ bg: 'gray.100' }}
-                onClick={() => setIsOpen(true)}
-            >
-                <Flex align="center">
-                    <Icon as={GrAdd} mr="3" fontWeight="700" fontSize="20" />
-                    Create Community
-                </Flex>
-            </MenuItem>
+            <Box mt={3} mb={4}>
+                <Text
+                    pl={3}
+                    mb={1}
+                    fontSize="8pt"
+                    fontWeight={500}
+                    color="gray.500"
+                    textTransform="uppercase"
+                >
+                    Moderator
+                </Text>
+                {isLoading
+                    ? [1, 3, 4].map(key => (
+                          <Skeleton key={key} height="25px" p={2} mb={2} />
+                      ))
+                    : mySnippets
+                          .filter(snippet => !!snippet.isModerator)
+                          .map(snippet => (
+                              <CommunityMenuItem
+                                  key={snippet.communityID}
+                                  icon={FaReddit}
+                                  iconColor="blue.500"
+                                  link={`/r/${snippet.communityID}`}
+                                  name={`/r/${snippet.communityID}`}
+                                  imageURL={snippet.imageURL}
+                              />
+                          ))}
+            </Box>
+            <Box mt={3} mb={4}>
+                <Text
+                    pl={3}
+                    mb={1}
+                    fontSize="8pt"
+                    fontWeight={500}
+                    color="gray.500"
+                    textTransform="uppercase"
+                >
+                    My communities
+                </Text>
+                <MenuItem
+                    width="100%"
+                    fontSize="11pt"
+                    _hover={{ bg: 'gray.100' }}
+                    onClick={() => setIsOpen(true)}
+                >
+                    <Flex align="center">
+                        <Icon
+                            as={GrAdd}
+                            mr="3"
+                            fontWeight="700"
+                            fontSize="20"
+                        />
+                        Create Community
+                    </Flex>
+                </MenuItem>
+                {isLoading
+                    ? [1, 3, 4].map(key => (
+                          <Skeleton key={key} height="25px" p={2} mb={2} />
+                      ))
+                    : mySnippets.map(snippet => (
+                          <CommunityMenuItem
+                              key={snippet.communityID}
+                              icon={FaReddit}
+                              iconColor="blue.500"
+                              link={`/r/${snippet.communityID}`}
+                              name={`/r/${snippet.communityID}`}
+                              imageURL={snippet.imageURL}
+                          />
+                      ))}
+            </Box>
         </>
     )
 }
