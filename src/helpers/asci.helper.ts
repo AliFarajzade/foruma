165384862bf9416d22977b4b0b30f0ca7ba45da4
Ali <alifarajzade1@yahoo.com>
2022-06-13 13:40:12 +0430

    Update CSS.

--- src/helpers/asci.helper.ts
+++ src/helpers/asci.helper.ts
@@ -1,7 +1,7 @@
 export const printASCII = () => {
     console.log(
         `%c𝗕𝘂𝗶𝗹𝘁 𝗯𝘆 © 𝗔𝗹𝗶 𝗙𝗮𝗿𝗮𝗷𝘇𝗮𝗱𝗲 𝟮𝟬𝟮𝟮\n`,
-        'font-size: 25px; color: pink;'
+        'font-size: 25px; color: coral;'
     )
     console.log(
         `%c𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗼𝗱𝗲: https://github.com/alifarajzade/foruma`,
