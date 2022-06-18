c23f47ba96da459fc83675aa8185e72b47b7ae97
Ali <alifarajzade1@yahoo.com>
2022-06-18 12:42:36 +0430

    Add general 404 page.

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import NotFound from '../components/not-found/not-found.component'
+
+const GeneralNotFound = () => <NotFound message="This page does not exist." />
+
+export default GeneralNotFound
