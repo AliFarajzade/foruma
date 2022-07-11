145f7a6543f56911f10c19558662c8577bf687f2
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-11 19:25:08 +0430

    Add title and meta tag.

--- src/pages/r/[communityID]/submit.tsx
+++ src/pages/r/[communityID]/submit.tsx
@@ -4,33 +4,41 @@ import AboutCommunity from '../../../components/community/about-community.compon
 import CommunityPageLayout from '../../../components/layout/community-layout.component'
 import NewPostForm from '../../../components/post/new-post-form.component'
 import PostSkeleton from '../../../components/post/post-skeleton.component'
+import SEO from '../../../components/SEO/seo.component'
 import useCommunityData from '../../../hooks/use-community-data.hook'
 
 const SubmitPage: NextPage = () => {
     const { communityState } = useCommunityData()
     return (
-        <CommunityPageLayout>
-            <>
-                <Box bg="white" padding="15px" fontWeight="400">
-                    Create a new post
-                </Box>
-                <NewPostForm
-                    communityImageURL={
-                        communityState.currentCommunity?.imageURL
-                    }
-                />
-            </>
-
-            <>
-                {communityState.currentCommunity ? (
-                    <AboutCommunity
-                        currentCommunity={communityState.currentCommunity}
+        <>
+            <SEO
+                description="Foruma Home Page, Discover lateest and hottest news in here."
+                image="/images/foruma.png"
+                title="New Post"
+            />
+            <CommunityPageLayout>
+                <>
+                    <Box bg="white" padding="15px" fontWeight="400">
+                        Create a new post
+                    </Box>
+                    <NewPostForm
+                        communityImageURL={
+                            communityState.currentCommunity?.imageURL
+                        }
                     />
-                ) : (
-                    <PostSkeleton />
-                )}
-            </>
-        </CommunityPageLayout>
+                </>
+
+                <>
+                    {communityState.currentCommunity ? (
+                        <AboutCommunity
+                            currentCommunity={communityState.currentCommunity}
+                        />
+                    ) : (
+                        <PostSkeleton />
+                    )}
+                </>
+            </CommunityPageLayout>
+        </>
     )
 }
 
